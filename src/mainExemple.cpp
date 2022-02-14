900ec9ee53660ffe35dc229c5b6478ef065e5728
tomdcht <tom.dechanet@gmail.com>
2022-02-14 18:35:23 +0100

    .gitignore + mainExemple

--- src/mainExemple.cpp
+++ src/mainExemple.cpp
@@ -2,6 +2,15 @@
 
 int main() {
 
+    Pixel bleu(0,0,255);
+    Pixel blanc(255,255,255);
+    Pixel rouge(255,0,0);
+
     Image im1(3,2);
+    im1.effacer(blanc);
+    im1.sauver("./data/white.ppm");
+    im1.dessinerRectangle(0,0, 2, 1, bleu);
+    im1.afficherConsole();
+    im1.ouvrir("./data/white.ppm");
 
 }
